8d81c14563190a3a22e0e0bfa2029f36d98d6ac9
supthunder <ejohn3@umbc.edu>
2016-05-04 05:32:48 -0400

    messing around with adding options with js
    need to add jquery to getJson in sample.js

---
+++ team12/sample/sample.js
@@ -0,0 +1,246 @@
+var classes = [
+    {
+      "type": "CMSC",
+      "number": 201,
+      "title": "Computer Science I for Majors",
+      "description": "An introduction to computer science through problem solving and computer programming.",
+      "credit": 4,
+      "unlock": [
+        "CMSC202"
+      ],
+      "prereq": 0,
+      "x": "61px",
+      "y": "84px"
+    },
+    {
+      "type": "CMSC",
+      "number": 202,
+      "title": "Computer Science II for Majors",
+      "description": "This course continues the development of the ability of the student to program and problem-solve by providing an introduction to object-oriented design and programming (OOP).",
+      "credit": 4,
+      "unlock": [
+        "CMSC304",
+        "CMSC313",
+        "CMSC331",
+        "CMSC341",
+        "CMSC451",
+        "CMSC484"
+      ],
+      "prereq": 1,
+      "x": "203px",
+      "y": "228px"
+    },
+    {
+      "type": "CMSC",
+      "number": 203,
+      "title": "Discrete Structures",
+      "description": "This course introduces the fundamental tools, topics and concepts of discrete mathematics needed to study computer science.",
+      "credit": 3,
+      "unlock": [
+        "CMSC313",
+        "CMSC331",
+        "CMSC341",
+        "CMSC451",
+        "CMSC452",
+        "CMSC457"
+      ],
+      "prereq": 0,
+      "x": "415.5px",
+      "y": "341.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 291,
+      "title": "Special Topics in Computer Science",
+      "description": "Special Topics in Computer Science to be announced.",
+      "credit": 4,
+      "unlock": [],
+      "prereq": 0,
+      "x": "585px",
+      "y": "240px"
+    },
+    {
+      "type": "CMSC",
+      "number": 299,
+      "title": "Independent Study in Computer Science",
+      "description": "A student may enroll in this course to study computer science topics that are not available in a regular course.",
+      "credit": 4,
+      "unlock": [],
+      "prereq": 0,
+      "x": "491px",
+      "y": "100px"
+    },
+    {
+      "type": "CMSC",
+      "number": 304,
+      "title": "Social and Ethical Issues in Information Technology",
+      "description": "A survey course that reviews social issues and the ethical impact of information technology throughout the world.",
+      "credit": 3,
+      "unlock": [],
+      "prereq": 1,
+      "x": "91.5px",
+      "y": "311.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 313,
+      "title": "Computer Organization and Assembly Language Programming",
+      "description": "This course introduces the student to the low-level abstraction of a computer system from a programmer�s point of view, with an emphasis on low-level programming.",
+      "credit": 3,
+      "unlock": [
+        "CMSC411",
+        "CMSC421",
+        "CMSC431",
+        "CMSC435"
+      ],
+      "prereq": 2,
+      "x": "127.5px",
+      "y": "449.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 331,
+      "title": "Principles of Programming Language",
+      "description": "This course examines the theory, design and implementation of programming languages and provides students with an introduction to programming languages that are likely to be new to them.",
+      "credit": 3,
+      "unlock": [
+        "CMSC431",
+        "CMSC432"
+      ],
+      "prereq": 2,
+      "x": "241.5px",
+      "y": "338.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 341,
+      "title": "Data Structures",
+      "description": "An examination of a range of advanced data structures, with an emphasis on an object-oriented approach.",
+      "credit": 3,
+      "unlock": [
+        "CMSC345",
+        "CMSC404",
+        "CMSC421",
+        "CMSC427",
+        "CMSC431",
+        "CMSC432",
+        "CMSC435",
+        "CMSC437",
+        "CMSC441",
+        "CMSC443",
+        "CMSC447",
+        "CMSC453",
+        "CMSC455",
+        "CMSC456",
+        "CMSC461",
+        "CMSC471",
+        "CMSC481",
+        "CMSC486",
+        "CMSC498",
+        "CMSC499"
+      ],
+      "prereq": 2,
+      "x": "726.5px",
+      "y": "657.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 345,
+      "title": "Software Design and Development",
+      "description": "This course introduces the basic concepts of software engineering, including software life cycle, requirements analysis and software design methods.",
+      "credit": 3,
+      "unlock": [
+        "CMSC448"
+      ],
+      "prereq": 1,
+      "x": "730px",
+      "y": "855px"
+    },
+    {
+      "type": "CMSC",
+      "number": 404,
+      "title": " The History of Computers and Computing",
+      "description": "This course covers the history of computers and the computing that was instrumental to the eventual widespread use of computers in developed countries.",
+      "credit": 3,
+      "unlock": [],
+      "prereq": 1,
+      "x": "803px",
+      "y": "964px"
+    },
+    {
+      "type": "CMSC",
+      "number": 411,
+      "title": "Computer Architecture",
+      "description": "This course covers the design of complex computer systems making heavy use of the components and techniques discussed in CMSC 313, CMPE 212 and CMPE 310.",
+      "credit": 3,
+      "unlock": [],
+      "prereq": 1,
+      "x": "83px",
+      "y": "575px"
+    },
+    {
+      "type": "CMSC",
+      "number": 421,
+      "title": "Principles of Operating Systems",
+      "description": "An introduction to the fundamentals of operating systems.",
+      "credit": 3,
+      "unlock": [
+        "CMSC426",
+        "CMSC483",
+        "CMSC487"
+      ],
+      "prereq": 2,
+      "x": "882.5px",
+      "y": "475.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 426,
+      "title": "Principles of Computer Security",
+      "description": "This course will provide an introduction to computer security, with specific focus on the computing aspects.",
+      "credit": 3,
+      "unlock": [],
+      "prereq": 0,
+      "x": "1062.5px",
+      "y": "173.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 427,
+      "title": "Wearable Computing",
+      "description": "This course covers fundamental concepts, methodologies, and algorithms related to wearable computing.",
+      "credit": 3,
+      "unlock": [],
+      "prereq": 1,
+      "x": "656px",
+      "y": "948px"
+    },
+    {
+      "type": "CMSC",
+      "number": 431,
+      "title": "Compiler Design Principles",
+      "description": "A detailed study of the design and implementation of a compiler for a high-level programming language.",
+      "credit": 3,
+      "unlock": [],
+      "prereq": 3,
+      "x": "308.5px",
+      "y": "466.5px"
+    },
+    {
+      "type": "CMSC",
+      "number": 432,
+      "title": "Object-Oriented Programming Languages and Systems",
+      "description": "This course covers the concepts of object-oriented programming (OOP) languages and systems, including an introduction to fundamental abstraction, modularity and encapsulation mechanisms in OOP from a software engineering and representational perspective.",
+      "credit": 3,
+      "unlock": [],
+      "prereq": 2,
+      "x": "372px",
+      "y": "570px"
+    }]
+	select = document.getElementById('selectm');
+  for (var i = 0; i < classes.length; i++) { 
+	var opt = document.createElement('option');
+    opt.value = classes[i].number;
+    opt.innerHTML = classes[i].type +" "+ classes[i].number;
+    select.appendChild(opt);
+  }
