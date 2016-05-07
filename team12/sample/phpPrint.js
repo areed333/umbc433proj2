1a338fc2529197b1b80ca8b8dcc40abc323ca138
elisha <ejohn3@umbc.edu>
2016-05-07 16:23:07 -0400

    what to do comments

--- team12/sample/phpPrint.js
+++ team12/sample/phpPrint.js
@@ -2,3 +2,28 @@ function sciClass(scclass){
 	console.log("all classes chosen:");
 	console.log(scclass);
 };
+
+
+// for each [i] in array ,delimeter it by commas. 
+// for each [i], for each class, print:
+	//  if lowest level in each obj, print "started"
+	//  else if highest level in category, print done
+	// else for each class print what is in unlock
+
+
+// categories:
+// From required Math Courses (MATH151, MATH152, MATH221)
+// Required Statistics Course (STAT355):
+// Required Science Track (Either BIOL100 + BIOL300, or BIOL141 + BIOL142, or 
+// CHEM101 + CHEM102, or PHYS121 + PHYS122):
+// Required Core Computer Science Courses (CMSC201, CMSC202, CMSC203, CMSC313,
+// CMSC331, CMSC341, CMSC411, CMSC421, CMSC441, and CMSC447):
+// Required Writing Intensive Computer Science Course (CMSC304):
+// Computer Science Electives - Four Total:
+
+/******************************* if anything do this first ****************************************/
+// Choose two from CMSC426, CMSC431, CMSC435, CMSC445, CMSC451, CMSC455, 
+// CMSC456, CMSC461, CMSC471, CMSC481, and CMSC483 as general electives. 
+// Then, choose any two additional 400-level Computer Science Courses as a 
+// technical electives. Note that CMSC404 and 495-499 do not qualify for this 
+// requirement. However, any from the previous list do qualify.
