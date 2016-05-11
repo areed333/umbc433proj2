9d856aa64ab5e19cb57a71e2d3ea9fa42c976d83
elisha <ejohn3@umbc.edu>
2016-05-11 01:47:29 -0400

    shortened code v1

--- team12/final/phpPrint.js
+++ team12/final/phpPrint.js
@@ -27,8 +27,8 @@
 	"any two additional 400-level Computer Science Courses as a technical electives. Note that CMSC404"+
 	"and 495-499 do not qualify for this requirement. However, any from the previous list do qualify.<br><br>";
 	
-	//classesToTake(classSections[3], CMSCtitle, classes_4xx, 'fCsElec', message);
-	cmscElectives();
+	classesToTake(classSections[3], CMSC_ELEC, classes_4xx, 'fCsElec', message);
+	//cmscElectives();
 
 function classesToTake(chosenClass, obj, allClassesArray, id, textToInsert){
 
@@ -53,6 +53,31 @@ function classesToTake(chosenClass, obj, allClassesArray, id, textToInsert){
 	// loop through each class selected
 	for(var i=0;i<difference.length;i++){
 		
+
+		// if 'fCsElec' or 'fsCsCore, then check all classes not just CMSC for requiremnts
+		if((id == 'fCsElec')|(id == 'fCsCore')){
+		// loops through all objects instead of just one
+		var objIDPre = "";
+		var objs = [CMSC,CMSC_REQ_ELEC,CMSC_ELEC,MATH,SCIENCE];
+		var start = 0;
+		while(objIDPre == ""){
+			console.log("Ojbs value: "+start);
+			var currentObj = objs[start];
+			objIDPre = $.grep(Object.keys(currentObj), function (k) { 
+			return currentObj[k].number == difference[i].substring(4); })
+			start += 1;
+			console.log("currentObj: "+difference[i]);
+			console.log("Ojbs value 2: "+start);
+			console.log("objIDPre: "+objIDPre);
+
+		}
+		var currentObj = objs[start - 1];
+		// check if it exists
+		if(typeof currentObj[objIDPre] !== "undefined"){
+			var preReqArray = currentObj[objIDPre].prereq;}
+
+		}
+		else{
 		// find id in Object
 		var objIDPre = $.grep(Object.keys(obj), function (k) { 
 			return obj[k].number == difference[i].substring(4); })
@@ -60,21 +85,34 @@ function classesToTake(chosenClass, obj, allClassesArray, id, textToInsert){
 		// check if it exists
 		if(typeof obj[objIDPre] !== "undefined"){
 			var preReqArray = obj[objIDPre].prereq;}
+		}
+
+
+		if((id == 'fCsElec')|(id == 'fCsCore')){
+			var checkPreReq = 0;
+			for(var a=0;a<preReqArray.length;a++){
+				// remove spaces from prereq since difference does not have space
+				var tempPreReq = preReqArray[a].replace(/\s+/g, '');
+				for(var b=0;b<classSections.length;b++){
+					console.log("class selections length: "+classSections.length);
+				// check if pre-req exists in user input classes
+				if(jQuery.inArray(tempPreReq, classSections[b]) !== -1){
+					checkPreReq += 1;
+				}
+			}
+		}
+
+	}
+		else{
 			// loop through preReqArray check if its taken
 			var checkPreReq = 0;
 			for(var a=0;a<preReqArray.length;a++){
 				var tempPreReq = preReqArray[a].replace(/\s+/g, '');
-
-				// for loop b
-				// checkPreReq = return
-
-				// if taken, += 1
 				if(jQuery.inArray(tempPreReq, chosenClass) !== -1){
 					checkPreReq += 1;
-				}
-
-				// end diff
+			}
 		}
+	}
 		// if all prereq are taken, array lenght should match
 		if(checkPreReq == preReqArray.length){
 			textToInsert += "-You could take "
@@ -148,19 +186,6 @@ function cmscElectives(){
 		var currentObj = objs[start - 1];
 		console.log("first object in currentObj:"+ currentObj[0].type + currentObj[0].number);
 		
-
-		// if(objIDPre == ""){
-		// 	objIDPre = $.grep(Object.keys(CMSC), function (k) { 
-		// 	return CMSC[k].number == difference[i].substring(4); })
-		// 	CMSC_REQ_ELEC = CMSC;
-		// 	console.log('ID2: '+objIDPre);
-		// }
-		// if(objIDPre == ""){
-		// 	objIDPre = $.grep(Object.keys(CMSC_ELEC), function (k) { 
-		// 	return CMSC_ELEC[k].number == difference[i].substring(4); })
-		// 	CMSC_REQ_ELEC = CMSC_ELEC;
-		// 	console.log('ID3: '+objIDPre);
-		// }
 
 		if(typeof currentObj[objIDPre] !== "undefined"){
 			var preReqArray = currentObj[objIDPre].prereq;}
